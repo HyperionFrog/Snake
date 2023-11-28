641b2409028a993001d9c781f8f4983a665b7de4
cbriice <150872450+cbriice@users.noreply.github.com>
2023-11-27 21:17:00 -0500

    Add files via upload

--- snake.m
+++ snake.m
@@ -1,124 +1,291 @@
 playAgain = true;
 while playAgain
-    scene_size = 24;
-    snake_scene = simpleGameEngine('sprites.png', 16, 16, 8, [245, 245, 245]);
-    
-    bottom_layer = ones(scene_size,scene_size);
-    top_layer = ones(scene_size,scene_size);
-    snake_pos_x = [6,6,6,6];
-    snake_pos_y = [8,7,6,5];
-    
-    acceptableInputs = ["uparrow"; "rightarrow"; "downarrow"; "leftarrow";
-                        "w"; "d"; "s"; 'a'];
-    
-    key = "d";
-    keyInput = "0";
-    snake_size = size(snake_pos_x, 2);
-    isHittingTheWall = false;
+scene_size = 24;
+snake_scene = simpleGameEngine('newsprites.png', 16, 16, 8, [245, 245, 245]);
 
-    notDied = true;
+bottom_layer = ones(scene_size,scene_size);
+top_layer = ones(scene_size,scene_size);
+snake_pos_x = [6,6,6,6];
+snake_pos_y = [8,7,6,5];
 
-    while notDied
-    
-        % Write snake positions
-        for i = 1:snake_size
-            top_layer(snake_pos_y(i), snake_pos_x(i)) = 285;
-        end
-    
-        drawScene(snake_scene, bottom_layer, top_layer)
-    
-        % Clear snake positions
-        for i = 1:snake_size
-            top_layer(snake_pos_y(i), snake_pos_x(i)) = 1;
-        end
-    
-        keyInput = getKeyboardInput(snake_scene, keyInput);
-        fprintf(keyInput + "\n");
-    
-        % Change direction only if keyInput is a valid input
-        % The first element in snake_pos should be the head
-        % The last element in snake_pos should be the tail
-        % During each loop, the tail changes from pos(end) to pos(end - 1)
-        % So pos(end) will be a dummy variable. We will use pos(end) to
-        % construct the next position of the head.
-        % Then use circshift to shift pos(end) to pos(1)
-        if ismember(keyInput, acceptableInputs)
-            key = keyInput;
-    
-            if key == "uparrow" || key == "w"
-                if snake_pos_y(1) > 1
-                    snake_pos_x(end) = snake_pos_x(1);
-                    snake_pos_y(end) = snake_pos_y(1) - 1;
-                else
-                    isHittingTheWall = true;
-                end
-            elseif key == "downarrow" || key == "s"
-                if snake_pos_y(1) < scene_size
-                    snake_pos_x(end) = snake_pos_x(1);
-                    snake_pos_y(end) = snake_pos_y(1) + 1;
-                else
-                    isHittingTheWall = true;
-                end
-            elseif key == "leftarrow" || key == "a"
-                if snake_pos_x(1) > 1
-                    snake_pos_x(end) = snake_pos_x(1) - 1;
-                    snake_pos_y(end) = snake_pos_y(1);
-                else
-                    isHittingTheWall = true;
-                end
-            elseif key == "rightarrow" || key == "d"
-                if snake_pos_x < scene_size
-                    snake_pos_x(end) = snake_pos_x(1) + 1;
-                    snake_pos_y(end) = snake_pos_y(1);
-                else
-                    isHittingTheWall = true;
-                end
-            end
-    
-            if isHittingTheWall
-                isHittingTheWall = false;
+acceptableInputs = ["uparrow", "rightarrow", "downarrow", "leftarrow",
+                    "w", "d", "s", 'a'];
+c = 0;
+key = "d";
+keyInput = "0";
+snake_size = size(snake_pos_x, 2);
+isHittingTheWall = false;
+notDied = true;
+
+while notDied
+
+    % Write snake positions
+    for i = 1:snake_size
+        top_layer(snake_pos_y(i), snake_pos_x(i)) = 3;
+    end
+
+    drawScene(snake_scene, bottom_layer, top_layer)
+
+    % Clear snake positions
+    for i = 1:snake_size
+        top_layer(snake_pos_y(i), snake_pos_x(i)) = 1;
+    end
+
+    keyInput = getKeyboardInput(snake_scene, keyInput);
+    fprintf(keyInput + "\n");
+
+    % Change direction only if keyInput is a valid input
+    % The first element in snake_pos should be the head
+    % The last element in snake_pos should be the tail
+    % During each loop, the tail changes from pos(end) to pos(end - 1)
+    % So pos(end) will be a dummy variable. We will use pos(end) to
+    % construct the next position of the head.
+    % Then use circshift to shift pos(end) to pos(1)
+    if ismember(keyInput, acceptableInputs)
+        key = keyInput;
+
+        if key == "uparrow" || key == "w"
+            if snake_pos_y(1) > 1
+                snake_pos_x(end) = snake_pos_x(1);
+                snake_pos_y(end) = snake_pos_y(1) - 1;
             else
-                snake_pos_x = circshift(snake_pos_x, 1);
-                snake_pos_y = circshift(snake_pos_y, 1);
+                isHittingTheWall = true;
+            end
+        elseif key == "downarrow" || key == "s"
+            if snake_pos_y(1) < scene_size
+                snake_pos_x(end) = snake_pos_x(1);
+                snake_pos_y(end) = snake_pos_y(1) + 1;
+            else
+                isHittingTheWall = true;
+            end
+        elseif key == "leftarrow" || key == "a"
+            if snake_pos_x(1) > 1
+                snake_pos_x(end) = snake_pos_x(1) - 1;
+                snake_pos_y(end) = snake_pos_y(1);
+            else
+                isHittingTheWall = true;
+            end
+        elseif key == "rightarrow" || key == "d"
+            if snake_pos_x < scene_size
+                snake_pos_x(end) = snake_pos_x(1) + 1;
+                snake_pos_y(end) = snake_pos_y(1);
+            else
+                isHittingTheWall = true;
             end
         end
-        
-        if applePosX == snake_pos_x && applePosY == snake_pos_y 
+         if isHittingTheWall
             notDied = false;
-        end
-        if isHittingTheWall
-            notDied = false;
+        else
+            snake_pos_x = circshift(snake_pos_x, 1);
+            snake_pos_y = circshift(snake_pos_y, 1);
         end
     end
-    
-    % Load sprite sheet
-    spriteSheetImage = imread('spritesheet.png');
-    
-    % Create red sprites
-    redSpriteImage = spriteSheetImage(1:24, 1:24, :);
-    redSprite = sge.createSprite(redSpriteImage);
-    
-    % Draw red sprites
-    for i = 1:25
-        for j = 1:25
-            sge.addSprite(redSprite, 24 * (i - 1), 24 * (j - 1));
-        end
+    c = c + 1;
+
+    if c == 18
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
     end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 36
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 54
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 72
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 90
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 108
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 126
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 144
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 162
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 180
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 198
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+    end
+    if c == 216
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+      if c == 234
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+      end
+      if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 252
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 270
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 288
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 306
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 324
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 342
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 360
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 378
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 396
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 414
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 432
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 450
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 468
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+    if c == 486
+       [applePosY, applePosX] = appleGen();
+       top_layer(applePosY, applePosX) = 2;
+    end
+    if ismember(applePosX, snake_pos_x) && ismember(applePosY, snake_pos_y)
+              notDied = false;
+        end
+end
+%playAgainScene = sge.createScene('Play Again?');
 
-    playAgainScene = sge.createScene('Play Again?');
-
-    playAgainGraph = sge.createGraph('Play Again? Graph', [0, 1, 2], [0, 1, 4]);
+ %   playAgainGraph = sge.createGraph('Play Again? Graph', [0, 1, 2], [0, 1, 4]);
     
-    playAgainGraph.setGraphProperty('title', 'Play Again? Graph');
-    playAgainGraph.setGraphProperty('xAxisLabel', 'X-Axis');
-    playAgainGraph.setGraphProperty('yAxisLabel', 'Y-Axis');
-    playAgainGraph.setGraphProperty('lineColor', [1, 0, 0]);
+  %  playAgainGraph.setGraphProperty('title', 'Play Again? Graph');
+   % playAgainGraph.setGraphProperty('xAxisLabel', 'X-Axis');
+    %playAgainGraph.setGraphProperty('yAxisLabel', 'Y-Axis');
+    %playAgainGraph.setGraphProperty('lineColor', [1, 0, 0]);
 
-    playAgainScene.addGraph(playAgainGraph);
-    
-    answer = input("Play again? (y for yes and n for no", "s");
+    %playAgainScene.addGraph(playAgainGraph);
+    close(snake_scene.my_figure);
+    answer = input("Play again? (y for yes and n for no): ", "s");
 
-    if answer.equals("no")
+    if answer == 'n'
         playAgain = false;
     end
 end
